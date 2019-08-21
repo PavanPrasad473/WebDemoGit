bef18a9d782cb3726e48479a2ee16384dad37a99
Pavan Prasad <pavan.sanka@smscountry.com>
2019-08-21 10:10:02 +0530

    commit

---
+++ WebGit/App1/Startup.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(App1.Startup))]
+namespace App1
+{
+    public partial class Startup {
+        public void Configuration(IAppBuilder app) {
+            ConfigureAuth(app);
+        }
+    }
+}
